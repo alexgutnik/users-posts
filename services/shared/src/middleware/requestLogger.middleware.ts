d86bb03472730f0253706af8b9b2c7a272917ff4
alex <gutnikalexandr@gmail.com>
2024-12-12 15:35:44 +0200

    moved shared module to the services

--- services/shared/src/middleware/requestLogger.middleware.ts
+++ services/shared/src/middleware/requestLogger.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import logger from "shared/utils/logger";
+import logger from "../utils/logger";
 
 /**
  * Middleware to log incoming requests.
